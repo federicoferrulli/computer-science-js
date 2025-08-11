e3836165652b4e61e59af9aa85079d3010bac9f9
federicoferrulli <federico.ferrulli01@gmail.com>
2025-08-12 01:18:28 +0200

    to implement remove HashTable

--- hashtable/HashTable.js
+++ hashtable/HashTable.js
@@ -1,14 +1,48 @@
-import LinkedList from "../linked-list/LinkedList.js";
-
 class HashTable {
-    constructor(){
+    constructor(num = 127){
+        this.table = [];
+        this.size = 0;
+        this.table.length = num; 
+        this.table.fill(structuredClone([]));
+    }  
+    
+    #_hash(key){
+        let sum = 0;
+        key = key.toString();
 
-    }   
-    get(){
-
+        for(let i=0; i < key.length; i++){
+            sum += key[i].charCodeAt(i);
+        }
+        return sum % this.table.length;
     }
-    set(){
 
+    set(key, val){
+        const index = this.#_hash(key);
+        this.table[index].push([key, val]);
+        this.size++;
     }
+
+    get(key){
+        const index = this.#_hash(key);
+        
+        const records = this.table[index];
+        for(let i=0; i<records.length;i++){
+            if(records[i] === key){
+                return records[i];
+            }
+        }
+        return undefined
+    }
+    remove(key){
+        const index = this.#_hash(key);
+        const records = this.table[index];
+        for(let i=0; i<records.length;i++){
+            if(records[i] === key){
+                return records[i];
+            }
+        }
+        return undefined
+    }
+    
 }
 export default HashTable;
